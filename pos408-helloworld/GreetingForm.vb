79525db5fbd1228036eff8ef5840ffd4492da314
Gordon Doskas <gordon2012@users.noreply.github.com>
2016-05-06 13:29:44 -0700

    initial commit

---
+++ pos408-helloworld/GreetingForm.vb
@@ -0,0 +1,44 @@
+﻿Public Class GreetingForm
+    Public Const HPADDING As Integer = 16
+    Public Const VPADDING As Integer = 40
+
+    Public Property vDir As Integer
+    Public Property hDir As Integer
+
+    Private Sub GreetingForm_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Me.CenterToScreen()
+        vDir = 1
+        hDir = 1
+    End Sub
+
+    Private Sub Timer1_Tick(sender As Object, e As EventArgs) Handles Timer1.Tick
+        Label1.Left += hDir
+        Label1.Top += vDir
+
+        If Label1.Top < 0 Then
+            vDir = 1
+            ChangeColor()
+        End If
+        If Label1.Top + Label1.Height > Label1.Parent.Height - VPADDING Then
+            vDir = -1
+            ChangeColor()
+        End If
+
+        If Label1.Left < 0 Then
+            hDir = 1
+            ChangeColor()
+        End If
+        If Label1.Left + Label1.Width > Label1.Parent.Width - HPADDING Then
+            hDir = -1
+            ChangeColor()
+        End If
+    End Sub
+
+    Private Sub ChangeColor()
+        Dim rand As New Random
+        Dim newColor As Color = Color.FromArgb(255, rand.Next(256), rand.Next(256), rand.Next(256))
+
+        Label1.ForeColor = newColor
+    End Sub
+
+End Class
---
+++ pos408-helloworld/HomeForm.vb
@@ -0,0 +1,24 @@
+﻿Public Class HomeForm
+    ' Simple Visual Basic Program
+    '        Gordon Doskas
+    '           POS/408
+    '          May 9, 2016
+    '        Dr. Bill Spees
+
+    ' This GUI program has a button that creates a new window with the word
+    ' Hello. The word moves diagonally, and when it hits the edge of the
+    ' screen, it reverses vertical or horizontal direction and changes to a
+    ' random color.
+
+    Private Sub HomeForm_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Me.CenterToScreen()
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Dim frmGreeting As New GreetingForm
+
+        Me.Hide()
+        frmGreeting.ShowDialog()
+        Me.Close()
+    End Sub
+End Class
